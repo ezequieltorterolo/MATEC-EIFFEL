f24a203369a2bf096eaa9705ac28b9620f5a68c2
ezequiel <Torteoloezequiel@gmail.com>
2024-11-10 14:46:06 -0300

    cambios

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+   // LOCAL
+   require_once "../vendor/rutex/autoload.php";
+
+   // en zonafranja.com
+   //require_once "/var/www/vhosts/zonafranja.com/rutexcore/v05.06/autoload.php";
+
+
+    
--- public/index.php
+++ public/index.php
@@ -1,5 +1,10 @@
-<?php
-   require_once "/var/www/vhosts/zonafranja.com/rutexcore/v05.06/autoload.php";
+ <?php
+ 
+ // LOCAL
+     require_once "../vendor/rutex/autoload.php";
+
+   // en zonafranja.com
+    //require_once "/var/www/vhosts/zonafranja.com/rutexcore/v05.06/autoload.php";
 
 
     
