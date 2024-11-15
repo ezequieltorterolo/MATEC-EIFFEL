045d659f1d10c1d302da64aec74d73964696c3e9
kareeennnn <anadeljardin690@gmail.com>
2024-11-15 00:51:23 -0300

    dejenme cocinar

--- app/views/sobreNosotros.php
+++ app/views/sobreNosotros.php
@@ -6,50 +6,59 @@
     <title>SOBRE NOSOTROS</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="styles/stylesobreNosotros.css" rel="stylesheet" type="text/css">
+    <link href="styles/style10.css" rel="stylesheet" type="text/css">
     <link href="styles/styles_general.css" rel="stylesheet" type="text/css">
 </head>
 <body>
     <?php include "segments/header.php" ?>
     <?php include "segments/nav.php" ?>
 
-    <div class="container my-5">
-        <div class="body-info">
-            <div class="seccion-titulo">
-                <h1 class="subtitulo">SOBRE EIFFEL IMPORTACIONES</h1>
+    <div class="container mt-5">
+
+  
+    <div class="container-fluid">
+        <div class="row">
+            <div id="seccion-imagen1" class="col-12">
+                <img class="img-fluid w-100 rounded" src="img/foto-empresa-eiffel.jpg" alt="Foto de Eiffel Importaciones">
+            </div>
+        </div>
+    </div>
+
+
+        
+            <div class="seccion-titulo mt-3 grueso5" id="megatitulo">
+                <h1> TU NUEVA DISTRUBIDORA DE CONFIANZA</h1>
                 <hr>
             </div>
-            <p>En Eiffel Importaciones, somos una distribuidora de Minas, Lavalleja con una sólida trayectoria en la venta y distribución de productos al por mayor. Nos especializamos en brindarles a las empresas una amplia variedad de productos de calidad a los mejores precios.</p>
+            <p id="texto">En Eiffel Importaciones, somos una distribuidora de Minas, Lavalleja con una sólida trayectoria en la venta y distribución de productos al por mayor. Nos especializamos en brindarles a las empresas una amplia variedad de productos de calidad a los mejores precios.</p>
         </div>
 
-        <div class="row">
-            <div class="col-md-8">
-                <div class="body-info">
-                    <div id="seccion-subtitulo">
+
+        <div class="row mx-2 mt-5 d-flex justify-content-center" id="columnas">
+       
+           
+                    <div class="col-md-3 col-12 p-3">
                         <h2>NUESTRA EXPERIENCIA</h2>
                         <hr>
-                        <p class="texto-subtitulo">Con varios años de experiencia, hemos evolucionado para atender a todo tipo de negocios de consumo dentro de la ciudad de manera confiable y rápida. Nos enorgullece ser el punto de conexión entre las mejores marcas y los mejores precios, brindando un surtido extenso y variado.</p>
+                        <p>Con varios años de experiencia, hemos evolucionado para atender a todo tipo de negocios de consumo dentro de la ciudad de manera confiable y rápida. Nos enorgullece ser el punto de conexión entre las mejores marcas y los mejores precios, brindando un surtido extenso y variado.</p>
                     </div>
-                    <div id="seccion-subtitulo">
+                    <div class="col-md-3 col-12 p-3">
                         <h2>CATÁLOGO VARIADO</h2>
                         <hr>
-                        <p class="texto-subtitulo">Estamos comprometidos con la expansión continua de nuestro catálogo para satisfacer las necesidades del mercado, siempre a la vanguardia de las tendencias y requerimientos de nuestros clientes.</p>
+                        <p >Estamos comprometidos con la expansión continua de nuestro catálogo para satisfacer las necesidades del mercado, siempre a la vanguardia de las tendencias y requerimientos de nuestros clientes.</p>
                     </div>
-                    <div id="seccion-subtitulo">
+                    <div class="col-md-3 col-12 p-3">
                         <h2>CONFIABILIDAD Y RAPIDEZ</h2>
                         <hr>
-                        <p class="texto-subtitulo">En Eiffel Importaciones, tratamos de dar el servicio más personalizado y cómodo posible. Nuestro equipo está disponible para asesorar a nuestros clientes a través de múltiples canales de comunicación: teléfono, correo electrónico y WhatsApp. Nos esforzamos para abastecer a nuestros clientes de forma rápida y flexible, incluso en momentos de alta demanda, asegurando que sus productos lleguen a destino en tiempo y forma.</p>
+                        <p>En Eiffel Importaciones, tratamos de dar el servicio más personalizado y cómodo posible. Nuestro equipo está disponible para asesorar a nuestros clientes a través de múltiples canales de comunicación: teléfono, correo electrónico y WhatsApp. Nos esforzamos para abastecer a nuestros clientes de forma rápida y flexible, incluso en momentos de alta demanda, asegurando que sus productos lleguen a destino en tiempo y forma.</p>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4">
-                <div id="seccion-imagen">
-                    <img src="../img/foto-empresa-eiffel.jpg" alt="Foto de Eiffel Importaciones">
-                </div>
-            </div>
+           
+            
+         
         </div>
 
-        <div class="body-info">
+       
             <div class="seccion-titulo">
                 <h1>CÓMO ENCARGAR</h1>
                 <hr>
@@ -63,7 +72,7 @@
                 <li>Recibirás una confirmación por e-mail y una notificación cuando tu pedido esté listo para la entrega.</li>
             </ul>
         </div>
-    </div>
+ 
 
     <?php include "segments/footer.php" ?>
 </body>
