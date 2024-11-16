4851c159aa6889a6338678f4e1f885a0a37c751f
kareeennnn <anadeljardin690@gmail.com>
2024-11-16 00:47:51 -0300

    Gestion usarios

--- app/views/admin/gestionUsuarios.php
+++ app/views/admin/gestionUsuarios.php
@@ -6,16 +6,101 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   <link href="../styles/styles_general.css" rel="stylesheet" type="text/css">
-  <link href="../styles/style8.css" rel="stylesheet" type="text/css">
+  <link href="../styles/style11.css" rel="stylesheet" type="text/css">
   <link href="../styles/popup.css" rel="stylesheet" type="text/css">
-  <script src="../scriptÑs/reservas.js"></script>
+  <script src="../script/reservas.js"></script>
   <meta charset="UTF-8" />
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <title>Eiffel Importaciones - Gestión de Usuarios</title>
 </head>
 
 <body>
-  <?php include "segments/header.php" ?>
 
+
+  <?php include "segments/header.php" ?>
+<div id="titlee" class="container">
+  <h1> Gestionar Usuarios  <hr></h1>
+</div>
+
+<div id="todo" class="container">
+
+
+<div class="row row-cols-1 row-cols-md-3 g-4">
+<div class="col-md-3">
+  <div class="card">
+ 
+  <div class="card-body">
+    <h5 class="card-title">NOMBRE</h5>
+    <h6 class="card-subtitle mb-2 text-muted">ID DE USUARIO</h6>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Correo electronico</li>
+    <li class="list-group-item">Telefono</li>
+    <li class="list-group-item">Direccion</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Suspender cuenta</a>
+    <a href="#" class="card-link">Modificar</a>
+  </div>
+</div>
+</div>
+<div class="col-md-3">
+  <div class="card">
+ 
+  <div class="card-body">
+    <h5 class="card-title">NOMBRE</h5>
+    <h6 class="card-subtitle mb-2 text-muted">ID DE USUARIO</h6>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Correo electronico</li>
+    <li class="list-group-item">Telefono</li>
+    <li class="list-group-item">Direccion</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Suspender cuenta</a>
+    <a href="#" class="card-link">Modificar</a>
+  </div>
+</div>
+</div>
+<div class="col-md-3">
+  <div class="card">
+ 
+  <div class="card-body">
+    <h5 class="card-title">NOMBRE</h5>
+    <h6 class="card-subtitle mb-2 text-muted">ID DE USUARIO</h6>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Correo electronico</li>
+    <li class="list-group-item">Telefono</li>
+    <li class="list-group-item">Direccion</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Suspender cuenta</a>
+    <a href="#" class="card-link">Modificar</a>
+  </div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+  
+</div>
+  </div>
+</div>
+    
+</div>
+</div>
+
+</div>
+</div>
 </body>
 <html>
