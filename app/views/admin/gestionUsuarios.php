a0e37b2e5af8a3037be67136d54463cb3fc62937
kareeennnn <anadeljardin690@gmail.com>
2024-11-16 00:03:59 -0300

    Gestion de usuarios pagina vacia

---
+++ app/views/admin/gestionUsuarios.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link href="../styles/styles_general.css" rel="stylesheet" type="text/css">
+  <link href="../styles/style8.css" rel="stylesheet" type="text/css">
+  <link href="../styles/popup.css" rel="stylesheet" type="text/css">
+  <script src="../scriptÑs/reservas.js"></script>
+  <meta charset="UTF-8" />
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <title>Eiffel Importaciones - Gestión de Usuarios</title>
+</head>
+
+<body>
+  <?php include "segments/header.php" ?>
+
+</body>
+<html>
