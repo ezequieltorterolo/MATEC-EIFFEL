7c05937d993267422f7c0e3270841a0c1288ce98
kareeennnn <anadeljardin690@gmail.com>
2024-11-18 01:46:15 -0300

    LA COSA DE USUARIO FINALMENTE

--- app/views/segments/nav.php
+++ app/views/segments/nav.php
@@ -7,6 +7,8 @@ use app\models\Categoria;
 
 
 <nav class="navegacion">
+
+<div id="pantalla-grande">
     <ul class="menu nav">
         
         <li class="nav-item elemento-lista">
@@ -27,6 +29,8 @@ use app\models\Categoria;
         <li class="nav-item elemento-lista"><a href="/sobreNosotros"> SOBRE NOSOTROS </a></li>
    
     </ul>
+
+                </div>
            
  
     <div class="iconos" style="display:none;">
@@ -39,7 +43,7 @@ use app\models\Categoria;
             <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
           </svg>
           <div id="badge-carrito" class="badge bg-danger" style="position: relative; right: 15px; bottom: 10px;">
-div>
+                </div>
                 </a>
         </button>
 
@@ -47,20 +51,21 @@ div>
 
         <?php if (isset($_SESSION["usuario"])): ?>
           <button>
-            <img src="img/user-solid.svg">
-            <div class="submenu-logueado-2">
-            <ul>
-                <li><p><?=$_SESSION["usuario"]["nombre"]?></p></li>
-                <li><a  href="/verReservas">Ver mis Reservas</a></li>
-                <li><a  onclick="cerrarSesion(<?=$_SESSION['usuario']['id']?>)">Cerrar sesion</a></li>
-            </ul>
+            <img src="img/user-solid.svg" onclick="abrirMenu()">
+            <div class="submenu-logueado">
+          
+                <li><p class="mt-2"><?=$_SESSION["usuario"]["nombre"]?></p></li>
+                <li><a class="esconder-nombre" style="color:#253141" href="/verReservas">Ver mis Reservas</a></li>
+                <li><a class="esconder-nombre" style=" color:#253141" onclick="cerrarSesion(<?=$_SESSION['usuario']['id']?>)">Cerrar sesion</a></li>
+           
+            
             </button> 
         </div>
        
         <?php else: ?>
         <button>
-        <a href="login" style="color:rgba(37,49,65,1);">
-            <img src="user-regular.svg">
+        <a href="login" >
+            <img src="img/user-regular.svg">
         </button> </a>
     </div>
     <?php endif?>
@@ -158,19 +163,13 @@ div>
         $("#sidebar").removeClass("active");
     });
 
+
+    
  
 });
-
-$(document).ready(function() {
-    
-    $('#lupa2').click(function() {
-        // Cierra el sidebar si está abierto
-        $("#sidebar").removeClass("active");
-        
-        // Alterna el buscador (abre/cierra)
-        $("#buscador2").toggleClass("active");
-    });
-
+function abrirMenu() {
+      $(".submenu-logueado").toggle();
+}
 
     function mostrarCantidadP() {
         let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
