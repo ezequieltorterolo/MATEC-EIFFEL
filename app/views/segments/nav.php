ceee327cba08536706e32b95711bbbf61f097420
kareeennnn <anadeljardin690@gmail.com>
2024-11-18 18:05:28 -0300

    comentario para eze

--- app/views/segments/nav.php
+++ app/views/segments/nav.php
@@ -42,7 +42,7 @@ use app\models\Categoria;
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
             <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
           </svg>
-          <div id="badge-carrito" class="badge bg-danger" style="position: relative; right: 15px; bottom: 10px;">
+          <div id="badge-carrito" class="badge bg-danger" style="position: relative; right: 15px; bottom: 10px;">  <!-- Aqui esta la badge de carrito en la vista movil-->
                 </div>
                 </a>
         </button>
