3f172cd3eb9dd36352375392e87b18506df1974a
kareeennnn <anadeljardin690@gmail.com>
2024-11-18 22:30:11 -0300

    footer

--- app/views/segments/footer.php
+++ app/views/segments/footer.php
@@ -1,28 +1,23 @@
 <footer>
-    <div class="footer-container">
-            <div class="logo">
+    <div class="footer container">
+        <div class="row ">
+            <div class="logo col-12 col-md-3 ">
                 <img src="img/logomenu.png" alt="Logo Eiffel" >
-                <p>Ayudando a las empresas a conseguir los mejores precios</p>
+               <p> Ayudando a las empresas a conseguir los mejores precios</p>
             </div>
-            <div class="contact-info">
+            <div class="contact-info col-md-3 col-6">
+                <h4>CONTACTO </h4>
                 <p><strong>Teléfono:</strong> <a href="tel:44404042">44404042</a></p><br>
                 <p><strong>Ubicación:</strong> <a href="#">Minas, Lavalleja</a></p>
             </div>
-            <div class="payment-methods">
-                <h4>MEDIOS DE PAGO</h4>
-                <div class="payment-icons">
-                    <span class="icon"></span>
-                    <span class="icon"></span>
-                    <span class="icon"></span>
-                    <span class="icon"></span>
-                </div>
-            </div>
-            <div class="about">
+           
+           
+            <div class="about col-md-3 col-6">
                 <h4>SOBRE EIFFEL</h4>
                 <a href="/sobreNosotros"><u>Sobre nosotros</a><br>
                 <a href="/sobreNosotros#comoEncargar">Cómo encargar</u></a>
             </div>
-            <div class="social">
+            <div class="social col-md-3 col-12 mt-md-0 mt-4">
                 <h4>SÍGUENOS EN</h4>
                 <div class="social-icons">
                     <a href="https://www.instagram.com/eiffel_importaciones?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw%3D%3D"><img src="img/icons8-instagram.svg" alt="Instagram"></a>
@@ -30,5 +25,5 @@
                 </div>
             </div>
     </div>
+
 </footer> 
-</footer>
