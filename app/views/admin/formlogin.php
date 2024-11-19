4652105d3bf064057c1c1187d0f6a1c0f990f41b
ezequiel <Torteoloezequiel@gmail.com>
2024-11-18 21:30:13 -0300

    loginadmin

--- app/views/admin/formlogin.php
+++ app/views/admin/formlogin.php
@@ -5,18 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario de Inicio de Sesión</title>
     <link href="styles/formlogin.css" rel="stylesheet" type="text/css">
-    <style>
-      
-    </style>
 </head>
 <body>
-
-<form method="POST" action="ValidarIngreso">
-    <h2>Iniciar Sesión</h2>
-    <input type="text" name="name" placeholder="Ingrese su nombre" required>
-    <input type="password" name="pass" placeholder="Ingrese su contraseña" required>
-    <button type="submit">Login</button>
-</form>
-
+    <img src="img/logito.png" alt="Logo">
+    <form method="POST" action="ValidarIngreso">
+        <h2>Iniciar Sesión</h2>
+        <input type="text" name="name" placeholder="Ingrese su nombre" required>
+        <input type="password" name="pass" placeholder="Ingrese su contraseña" required>
+        <button type="submit">Login</button>
+    </form>
 </body>
 </html>
